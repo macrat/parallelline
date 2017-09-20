28112bb8e07d3ae92a63e7a82d95aece409cde1a
MacRat <m@crat.jp>
2017-09-20 16:39:07 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='parallelline',
+      version='0.0.1',
+      description='A Framework for buffering heavy calculation (or I/O) with multiprocessing',
+      long_description=open('README.md').read(),
+      author='MacRat',
+      author_email='m@crat.jp',
+      url='http://github.com/macrat/parallelline',
+      py_modules=['parallelline'],
+      license='MIT License')
